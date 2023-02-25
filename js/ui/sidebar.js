007460b55b0847a37f55742561fc2d5089cacd1f
Manfred Stock <m-osm@nfred.ch>
2023-02-25 17:44:18 +0100

    Support changes to the comment filter using sidebar

--- js/ui/sidebar.js
+++ js/ui/sidebar.js
@@ -5,8 +5,11 @@ class Sidebar {
         this.context = context;
         this.application = document.body;
         this.sidebarButton = document.getElementById('sidebar-button');
+        this.settingsForm = document.getElementById('settings-form');
         this.runTimeElement = document.getElementsByName('runTime')[0];
         this.runTimeElement.value = -1 * this.context.runTime / 1000;
+        this.commentElement = document.getElementsByName('comment')[0];
+        this.commentElement.value = this.context.comment;
     }
 
     initializeEventListeners() {
@@ -17,12 +20,31 @@ class Sidebar {
         this.runTimeElement.addEventListener('change', () => {
             this._updateRuntime(-1 * this.runTimeElement.value);
         });
+
+        this.settingsForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            this._updateComment(this.commentElement.value);
+        });
+        ['keyup', 'change'].forEach((eventName) => {
+            this.commentElement.addEventListener(eventName, (e) => {
+                this._updateComment(this.commentElement.value);
+            });
+        });
     }
 
     _updateRuntime(newRunTime) {
-        this.hashParams.set('runTime', newRunTime);
-        this.windowLocationObj.hash = '#' + this.hashParams.toString();
+        this._updateHash('runTime', newRunTime);
         this.context.runTime = newRunTime * 1000;
+    }
+
+    _updateComment(comment) {
+        this._updateHash('comment', comment);
+        this.context.comment = comment;
+    }
+
+    _updateHash(key, value) {
+        this.hashParams.set(key, value);
+        this.windowLocationObj.hash = '#' + this.hashParams.toString();
     }
 }
 
