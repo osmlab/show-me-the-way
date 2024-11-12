80afb0c6e58b6a7198023ffb8adb9416474585d7
iandees <ian.dees@gmail.com>
2024-11-12 18:46:19 +0000

    Deploying to gh-pages from @ osmlab/show-me-the-way@6e3751a97d77d496a387bafbe3468903b1e00244 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -82,21 +82,23 @@ class Change {
 
   isRelevant() {
     return new Promise(resolve => {
-      let relevant = false;
+      let commentRelevance = false;
+      let keyRelevance = false;
       const mapElement = this.neu || this.old;
 
-      if (this.context.comment == "") {
+      if (this.context.comment === "" && !this.context.key) {
         return resolve(true);
       }
 
       this.fetchChangesetData(mapElement.changeset).then(changesetData => {
-        relevant = changesetData.comment && changesetData.comment.toLowerCase().indexOf(this.context.comment.toLowerCase()) > -1;
+        commentRelevance = this.context.comment !== "" && changesetData.comment?.toLowerCase().includes(this.context.comment.toLowerCase()) || false;
+        keyRelevance = Object.keys(mapElement.tags).includes(this.context.key);
 
-        if (!relevant) {
-          console.log("Skipping map element " + mapElement.id + " because changeset " + mapElement.changeset + " didn't match " + this.context.comment);
+        if (!(commentRelevance || keyRelevance)) {
+          console.log("Skipping map element " + mapElement.id + " because it didn't match filters.");
         }
 
-        return resolve(relevant);
+        return resolve(commentRelevance | keyRelevance);
       });
     });
   }
