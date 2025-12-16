3d199ed3c4f02fd37fe108746b8e683ae5e8e236
iandees <ian.dees@gmail.com>
2025-12-16 16:47:47 +0000

    Deploying to gh-pages from @ osmlab/show-me-the-way@f3dfe697c12e8a334444057040353c9e27d35b55 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -4559,25 +4559,39 @@
       } else {
         this.overviewMap.setView(defaultCenter, 4);
       }
-      const mapboxKey = "pk.eyJ1Ijoib3BlbnN0cmVldG1hcHVzIiwiYSI6ImNqdTM1ZWxqeTBqa2MzeXBhODIxdnE2eG8ifQ.zyhAo181muDzPRdyYsqLGw";
-      new L.TileLayer(
-        "https://api.mapbox.com/styles/v1/openstreetmapus/{style_id}/tiles/256/{z}/{x}/{y}?access_token={key}",
-        {
-          style_id: "cju35gljt1bpm1fp2z93dlyca",
-          key: mapboxKey,
-          attribution: '<a href="https://mapbox.com/about/maps/">Terms &amp; Conditions</a>'
-        }
-      ).addTo(this.main);
-      new L.TileLayer(
-        "https://api.mapbox.com/styles/v1/openstreetmapus/{style_id}/tiles/256/{z}/{x}/{y}?access_token={key}",
-        {
+      const isLocal = window.location.hostname === "localhost" || window.location.hostname.startsWith("192");
+      if (isLocal) {
+        this.mainTileLayer = new L.TileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          crossOrigin: "anonymous"
+        }).addTo(this.main);
+        new L.TileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
           minZoom: 4,
           maxZoom: 8,
-          style_id: "cj8xtgojqhd3z2sorzpi01csj",
-          key: mapboxKey,
-          attribution: '<a href="https://mapbox.com/about/maps/">Terms &amp; Conditions</a>'
-        }
-      ).addTo(this.overviewMap);
+          attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.overviewMap);
+      } else {
+        const mapboxKey = "pk.eyJ1Ijoib3BlbnN0cmVldG1hcHVzIiwiYSI6ImNqdTM1ZWxqeTBqa2MzeXBhODIxdnE2eG8ifQ.zyhAo181muDzPRdyYsqLGw";
+        this.mainTileLayer = new L.TileLayer(
+          "https://api.mapbox.com/styles/v1/openstreetmapus/{style_id}/tiles/256/{z}/{x}/{y}?access_token={key}",
+          {
+            style_id: "cju35gljt1bpm1fp2z93dlyca",
+            key: mapboxKey,
+            attribution: '<a href="https://mapbox.com/about/maps/">Terms &amp; Conditions</a>',
+            crossOrigin: "anonymous"
+          }
+        ).addTo(this.main);
+        new L.TileLayer(
+          "https://api.mapbox.com/styles/v1/openstreetmapus/{style_id}/tiles/256/{z}/{x}/{y}?access_token={key}",
+          {
+            minZoom: 4,
+            maxZoom: 8,
+            style_id: "cj8xtgojqhd3z2sorzpi01csj",
+            key: mapboxKey,
+            attribution: '<a href="https://mapbox.com/about/maps/">Terms &amp; Conditions</a>'
+          }
+        ).addTo(this.overviewMap);
+      }
       this.main.attributionControl.setPrefix("");
       this.overviewMap.attributionControl.setPrefix(false);
       this.featureGroup = L.featureGroup().addTo(this.main);
